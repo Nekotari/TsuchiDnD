875846237236e4147699a6621c9fa6fb389c89cf
Runae <stella_nemesis@live.com>
2021-01-14 14:50:05 +0900

    Generated reservations

---
+++ db/migrate/20210114054843_create_reservations.rb
@@ -0,0 +1,14 @@
+class CreateReservations < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reservations do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :room, null: false, foreign_key: true
+      t.datetime :check_in
+      t.datetime :check_out
+      t.integer :price
+      t.integer :billing
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_12_090921) do
+ActiveRecord::Schema.define(version: 2021_01_14_054843) do
+
+  create_table "reservations", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "room_id", null: false
+    t.datetime "check_in"
+    t.datetime "check_out"
+    t.integer "price"
+    t.integer "billing"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_reservations_on_room_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string "property_type"
@@ -42,5 +55,7 @@ ActiveRecord::Schema.define(version: 2021_01_12_090921) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reservations", "rooms"
+  add_foreign_key "reservations", "users"
   add_foreign_key "rooms", "users"
 end
